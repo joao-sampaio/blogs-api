b332cf07f2438a2271603ffb1f483b40edad223c
joao-sampaio <joaoantoniosampaio04@outlook.com>
2022-09-25 17:31:47 -0300

    correções

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,24 @@
 const { users } = require('../services');
 
 const login = async (req, res) => {
-  try {
-    const {email, password} = req.params
-    const result = await users.login(email, password);
-    if (result.type) {
-      res.status(result.type).json({ message: result.message })
-    } else {
-      res.status(200).json(result);
-    }
-  } catch (e) {
-    res.status(500).json({ message: 'err' });
+  const { email, password } = req.body;
+  const result = await users.login(email, password);
+  if (result.type) {
+    res.status(result.type).json({ message: result.message });
+  } else {
+    res.status(200).json(result);
   }
+  // try {
+  //   const {email, password} = req.body
+  //   const result = await users.login(email, password);
+  //   if (result.type) {
+  //     res.status(result.type).json({ message: result.message })
+  //   } else {
+  //     res.status(200).json(result);
+  //   }
+  // } catch (e) {
+  //   res.status(500).json({ message: 'err' });
+  // }
 };
 
 module.exports = {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,24 +3,26 @@ const { User } = require('../models');
 // const validations = require('../middlewares/validations');
 
 const login = async (email, password) => {
-  if (email !== undefined && password !== undefined) {
-    const result = await User.findAll({
-      where: {email, password}
-    });
-    console.log(result)
-    if (!result) {
-      return {
-        type: 400,
-        message: 'Invalid fields'
-      }
-    }
-    return result;
-  } else {
+  if (!email || !password) {
     return {
       type: 400,
-      message: 'Some required fields are missing'
-    }
+      message: 'Some required fields are missing',
+    };
   }
+  console.log('testtestestestetstetstetstete');
+  // const result = await User.findOne({
+  //       where: {email, password}
+  //     });
+
+  const result = await User.findOne({ where: { email } });
+  console.log(result, email, password, 'sssssssssssssssssssssss');
+  if (!result) {
+    return {
+      type: 400,
+      message: 'Invalid fields',
+    };
+  }
+  return result;
 };
 
 module.exports = {
